78f57b39f7779c4955c0e726238d88c1caaf3ff5
Aizen-art-hash <catedrillaardy123@gmail.com>
2025-04-25 17:30:37 +0800

    TP6.java

--- TP6.java
+++ TP6.java
@@ -7,27 +7,30 @@ public class TP6 {
     public static void main(String[] args) {
      
         Scanner s = new Scanner(System.in);
-        String word1, word2, sub;
 
         System.out.print("Enter the first word: ");
-        word1 = s.nextLine();
+        String word1 = s.nextLine();
 
         System.out.print("Enter the second word: ");
-        word2 = s.nextLine();
+        String word2 = s.nextLine();
 
-        sub = word1.length() >= 2 ? word1.substring(word1.length() - 2) : word1;
+        String sub = word1.length() >= 2
+                ? word1.substring(word1.length() - 2)
+                : word1;
 
-        Pattern p = Pattern.compile("[a-zA-Z]{1,2}" + sub + "$");
+        Pattern p = Pattern.compile("([a-zA-Z]{1,2})" + sub + "$");
         Matcher m = p.matcher(word2);
-
         if (m.matches()) {
-            System.out.println(word2 + " rhymes with " + word1);
-        } else if (word2.length() > sub.length()) {
+            if (m.group(1).length() == 2) {
+                System.out.println(word2 + " rhymes with " + word1);
+            } else {
+                System.out.println(word2 + " does not rhyme with " + word1);
+            }
+        } else if (word2.endsWith(sub)) {
             System.out.println("I'm not sure! Sorry!");
         } else {
             System.out.println(word2 + " does not rhyme with " + word1);
         }
 
-        
     }
 }
