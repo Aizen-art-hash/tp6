a88243f6ed1dd9d7f5bf5c545475f6245c26cf0d
Aizen-art-hash <catedrillaardy123@gmail.com>
2025-05-22 23:15:45 +0800

    Add files via upload

--- TriPrismDemo.java
+++ TriPrismDemo.java
@@ -1,21 +1,13 @@
 
-import java.util.Scanner;
-
 public class TriPrismDemo {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
         TriPrism<Integer> tp1 = new TriPrism<>();
-        System.out.println("Enter values for Triangular Prism:");
-        System.out.print("Length: ");
-        tp1.setLength(scanner.nextInt());
-        System.out.print("Base: ");
-        tp1.setBase(scanner.nextInt());
-        System.out.print("Height: ");
-        tp1.setHeight(scanner.nextInt());
-        System.out.println("\nTriangular Prism 1");
+        tp1.setLength(10);
+        tp1.setBase(7);
+        tp1.setHeight(5);
+        System.out.println("Triangular Prism 1");
         System.out.println("Length: " + tp1.getLength());
         System.out.println("Base: " + tp1.getBase());
         System.out.println("Height: " + tp1.getHeight());
@@ -23,19 +15,14 @@ public class TriPrismDemo {
         System.out.println();
 
         TriPrism<Double> tp2 = new TriPrism<>();
-        System.out.println("Enter values for Triangular Prism 2:");
-        System.out.print("Length: ");
-        tp2.setLength(scanner.nextDouble());
-        System.out.print("Base: ");
-        tp2.setBase(scanner.nextDouble());
-        System.out.print("Height: ");
-        tp2.setHeight(scanner.nextDouble());
-        System.out.println("\nTriangular Prism 2");
+        tp2.setLength(9.10);
+        tp2.setBase(6.25);
+        tp2.setHeight(4.20);
+        System.out.println("Triangular Prism 2");
         System.out.printf("Length: %.2f\n", tp2.getLength());
         System.out.printf("Base: %.2f\n", tp2.getBase());
         System.out.printf("Height: %.2f\n", tp2.getHeight());
         System.out.printf("The volume of the triangular prism is %.4f\n", tp2.calculateVolume());
-        
-        scanner.close();
     }
 }
+   
