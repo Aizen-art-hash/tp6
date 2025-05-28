a71f66450dc613e00893f6ba8d6c0e033ccf7361
Aizen-art-hash <catedrillaardy123@gmail.com>
2025-05-28 12:00:44 +0800

    MyThread.java

--- MyThread.java
+++ MyThread.java
@@ -1,11 +1,29 @@
 
-public class MyThread extends Thread{
-    public MyThread(String name) {
-        super(name);
+public class TriPrism<T extends Number> {
+    private T length;
+    private T base;
+    private T height;
+
+    public void setValues(T length, T base, T height) {
+        this.length = length;
+        this.base = base;
+        this.height = height;
     }
 
-    public void run() {
-        System.out.println(getName() + " is " + getState());
+    public T getLength() {
+        return length;
+    }
+
+    public T getBase() {
+        return base;
+    }
+
+    public T getHeight() {
+        return height;
+    }
+
+    public double calculateVolume() {
+        return length.doubleValue() * (0.5 * base.doubleValue() * height.doubleValue());
     }
 }
-
+    
